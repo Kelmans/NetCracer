1894cd52b3c220cda8f2bfb3a1ad19c3b975fa83
vlba0519 <baidakovpo53@mail.ru>
2019-06-03 12:51:56 +0400

    RGB

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         var flag_km = 0;//Флаг нажатия для #km
         var flag_lm = 0;//Флаг нажатия для #lm
         // при клике по диву, делаем проверку
-        $(document).on('click', '.modifiedCheckbox', function () {
+        $(document).on('click', '.table__modified-checkbox', function () {
             var checkbox = $(this).find('input[type=checkbox]');
             // если чекбокс был активен
             if (checkbox.prop("checked")) {
@@ -31,16 +31,16 @@ $(document).ready(function() {
         $(document).on('click', '#box', function () { //Активация всех чекбоксов
             var manyCheckboxs = $(this).find('input[type=checkbox]');
             if (manyCheckboxs.prop("checked")) { //включен
-                $('.modifiedCheckbox input:checkbox').prop('checked', true);
-                $('.modifiedCheckbox').not(this).addClass("check_active");
+                $('.table__modified-checkbox input:checkbox').prop('checked', true);
+                $('.table__modified-checkbox').not(this).addClass("check_active");
 
             } else { //выключен
-                $('.modifiedCheckbox input:checkbox').prop('checked', false);
-                $('.modifiedCheckbox').not(this).removeClass("check_active");
+                $('.table__modified-checkbox input:checkbox').prop('checked', false);
+                $('.table__modified-checkbox').not(this).removeClass("check_active");
 
             }
         });
-        $(document).on('click', '.modifiedCheckbox', function () {
+        $(document).on('click', '.table__modified-checkbox', function () {
             var inps = document.getElementsByTagName('tr');
             var count = $('.inp[type=checkbox]:checked').length;
             if (count == inps.length - 1) {
@@ -53,21 +53,21 @@ $(document).ready(function() {
             if (counterAdditionLineage == 0) {
                 $('#bossTable')
                     .append($('<tr class="remove_class">')
-                        .append($('<td>').append($('<div class="modifiedCheckbox der">').append($('<span>').append($('<input type="checkbox" class="inp" >')))))
-                        .append($('<td>').append($('<div class="nameLine modifiedFont">').append($('<div class="text_Line">').append('<input type=text  id="nameLine"class="inpt inpt1">'), $('<div class="fasten">'))))
+                        .append($('<td>').append($('<div class="table__modified-checkbox der">').append($('<span>').append($('<input type="checkbox" class="inp" >')))))
+                        .append($('<td>').append($('<div class="table__name-line modifiedFont">').append($('<div class="text_Line">').append('<input type=text  id="table__name-line"class="inpt inpt1">'), $('<div class="fasten">'))))
                         .append($('<td>').append($('<div class="statusLine modifiedFont">').append($('<div>').append('<input type=text id="statusLine"class="inpt inpt2">'))))
                         .append($('<td>').append($('<div class="autLine modifiedFont">').append($('<div>').append('<input type=text id="autLine"  class="inpt inpt3">'))))
                         .append($('<td>').append($('<div class="loginLine modifiedFont">').append($('<div>').append('<input type=text id="loginLine" class="inpt inpt4">'))))
                         .append($('<td>').append($('<div class="container_img">').append($('<div id="qwe" class="container_img-item2">'))))
                     )
-                $('.modifiedCheckbox input:checkbox').prop('checked', false);
-                $('.modifiedCheckbox').not(this).removeClass("check_active");
-                $('div.nameLine').addClass('nameLine');
+                $('.table__modified-checkbox input:checkbox').prop('checked', false);
+                $('.table__modified-checkbox').not(this).removeClass("check_active");
+                $('div.table__name-line').addClass('table__name-line');
                 $.each(rowsOne, function (index, row) {
                     $('#bossTable').children('tbody').append(row);
                 });
-                $('.containerArrow').removeClass('containerArrowSortDec');
-                $('.containerArrow').removeClass('containerArrowSortInc');
+                $('.table__container-arrow').removeClass('containerArrowSortDec');
+                $('.table__container-arrow').removeClass('containerArrowSortInc');
                 counterAdditionLineage++;
             } else {
                 alert("Заполните предыдущую строку и нажмите 'ОК' ");
@@ -76,18 +76,18 @@ $(document).ready(function() {
         });
         $(document).on('click', '.container_img-item2', function () {// изменить имена
 
-            if (document.getElementById("nameLine").value === '' || document.getElementById("statusLine").value === '' || document.getElementById("autLine").value === '' || document.getElementById("loginLine").value === '') {
+            if (document.getElementById("table__name-line").value === '' || document.getElementById("statusLine").value === '' || document.getElementById("autLine").value === '' || document.getElementById("loginLine").value === '') {
                 alert("Заполните все поля")
             } else {
                 //Добавляю html код
                 //Создал переменные для хранения значений из инпутов
                 //У ново добавленого штмл кода убираю дисплэй нон и добавляю его инпутам
                 //Сделал для сортировки массива
-                var inputNameLine = document.getElementById("nameLine").value;
-                var inputStatusLine = document.getElementById("nameLine").value;
-                var inputAutLine = document.getElementById("nameLine").value;
+                var inputNameLine = document.getElementById("table__name-line").value;
+                var inputStatusLine = document.getElementById("table__name-line").value;
+                var inputAutLine = document.getElementById("table__name-line").value;
                 var inputLoginLine = document.getElementById("loginLine").value;
-                var as = '<tr class="add_class dis"><td><div class="modifiedCheckbox der"><span> <input type="checkbox" class="inp"></span></div></td><td><div class="nameLine"><div  class="text_Line">' + inputNameLine + '</div> <div class="fasten"></div></div></td> <td><div class="statusLine">' + inputStatusLine + '</div></td> <td><div class="autLine">' + inputAutLine + '</div></td> <td><div class="loginLine">' + inputLoginLine + '</div></td> <td><div class="container_img"><div class="container_img-item"></div></div></td> </tr>'
+                var as = '<tr class="add_class dis"><td><div class="table__modified-checkbox der"><span> <input type="checkbox" class="inp"></span></div></td><td><div class="table__name-line"><div  class="text_Line">' + inputNameLine + '</div> <div class="fasten"></div></div></td> <td><div class="statusLine">' + inputStatusLine + '</div></td> <td><div class="autLine">' + inputAutLine + '</div></td> <td><div class="loginLine">' + inputLoginLine + '</div></td> <td><div class="container_img"><div class="container_img-item"></div></div></td> </tr>'
                 $('#bossTable').append(as);
                 $(this).addClass("container_img-item");
                 $('input[type=text]').prop('disabled', true);
@@ -118,16 +118,16 @@ $(document).ready(function() {
                 alert('Закончите с редактированием новой строки');
             }
         });
-        $('.containerArrow').click(function (event) {
+        $('.table__container-arrow').click(function (event) {
 
             var element_click = event.target.className; // Тут имя класса по которому тыкнули
-            var parent_click_element = $('.containerArrow').closest(".modifiedFont");
+            var parent_click_element = $('.table__container-arrow').closest(".modifiedFont");
             var rods = event.target.closest('.modifiedFont');
             var nameClass = '.' + rods.className;
             var rod = event.target.closest('th').id;// тут ID тега TH
             var id_element;
 
-            if (element_click == 'containerArrow' || element_click == 'containerArrow containerArrowSortDec' || element_click == 'containerArrow containerArrowSortDec containerArrowSortInc') {
+            if (element_click == 'table__container-arrow' || element_click == 'table__container-arrow containerArrowSortDec' || element_click == 'table__container-arrow containerArrowSortDec containerArrowSortInc') {
                 $('#bossTable td').removeClass('fontWe');
                 if (rod == 'sl') {
                     var n = 1;
@@ -142,22 +142,22 @@ $(document).ready(function() {
                     var n = 4
                     flag_lm++;
                 }
-                if (element_click == 'containerArrow') {
+                if (element_click == 'table__container-arrow') {
                     counterСlicks = 0;
-                } else if (element_click == 'containerArrow containerArrowSortDec') {
+                } else if (element_click == 'table__container-arrow containerArrowSortDec') {
                     counterСlicks = 1;
                 } else {
                     counterСlicks = 2;
                 }
 
                 if (counterСlicks == 0) {
-                    $('.containerArrow').removeClass('containerArrowSortDec');
-                    $('.containerArrow').removeClass('containerArrowSortInc');
+                    $('.table__container-arrow').removeClass('containerArrowSortDec');
+                    $('.table__container-arrow').removeClass('containerArrowSortInc');
                     constSort *= -1;
                     $(event.target).addClass('containerArrowSortDec');
                     sortTable(constSort, n);
                 } else if (counterСlicks == 1) {
-                    $('.containerArrow').removeClass('containerArrowSortInc');
+                    $('.table__container-arrow').removeClass('containerArrowSortInc');
                     $(event.target).addClass('containerArrowSortInc');
                     constSort *= -1;
                     sortTable(constSort, n);
@@ -174,7 +174,7 @@ $(document).ready(function() {
                 var rows = $('#bossTable tbody  tr').get();
                 ``
                 rows.sort(function (a, b) {
-                    $('.containerArrow').parents('.modifiedFont').addClass('fontWe');
+                    $('.table__container-arrow').parents('.modifiedFont').addClass('fontWe');
                     var A = getVal(a);
                     var B = getVal(b);
 
