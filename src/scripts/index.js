3288b4cb51cc376d0067f4a8563d1d10283506e7
vlba0519 <baidakovpo53@mail.ru>
2019-06-04 15:50:31 +0400

    RGB

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         var counterAdditionLineage = 0;
         var counterСlicks = 0;
         var constSort = 1;
-        var rowsOne = $('#bossTable tbody  tr').get();
+        var rowsOne = $('#onlyTable tbody  tr').get();
         var flag_sl = 0;// Флаг нажатия для #sl
         var flag_nm = 0;// Флаг нажатия для #nm
         var flag_km = 0;//Флаг нажатия для #km
@@ -42,32 +42,32 @@ $(document).ready(function() {
         });
         $(document).on('click', '.table__checkbox-wrapper', function () {
             var variableRow = document.getElementsByTagName('tr');
-            var countActiveCheckbox = $('.inp[type=checkbox]:checked').length;
+            var countActiveCheckbox = $('.table__input-checkbox[type=checkbox]:checked').length;
             if (countActiveCheckbox == variableRow.length - 1) {
                 $('#box').addClass("check_active");
             } else {
                 $('#box').removeClass("check_active");
             }
         });
-        $('#addition-lineage').click(function () {
+        $('#addition-row').click(function () {
             if (counterAdditionLineage == 0) {
-                $('#bossTable')
+                $('#onlyTable')
                     .append($('<tr class="remove_class">')
-                        .append($('<td class="table-body__cell">').append($('<div class="table__checkbox-wrapper der">').append($('<span>').append($('<input type="checkbox" class="inp" >')))))
-                        .append($('<td class="table-body__cell">').append($('<div class="table-body__name-line font-weight_bold">').append($('<div class="text_Line">').append('<input type=text  id="table-body__name-line"class="search-input search-input__one-cell " >'), $('<div class="table-body__visible-pin">'))))
-                        .append($('<td class="table-body__cell">').append($('<div class="table-body__status-line font-weight_bold">').append($('<div>').append('<input type=text id="table-body__status-line"class="search-input search-input__two-cell ">'))))
-                        .append($('<td class="table-body__cell">').append($('<div class="table-body__aut-line font-weight_bold">').append($('<div>').append('<input type=text id="table-body__aut-line"  class="search-input search-input__three-cell ">'))))
-                        .append($('<td class="table-body__cell">').append($('<div class="table-body__login-line font-weight_bold">').append($('<div>').append('<input type=text id="table-body__login-line" class="search-input search-input__four-cell ">'))))
+                        .append($('<td class="table-body__cell">').append($('<div class="table__checkbox-wrapper der">').append($('<span>').append($('<input type="checkbox" class="table__input-checkbox" >')))))
+                        .append($('<td class="table-body__cell">').append($('<div class="table-body__text-cell font-color">').append($('<div class="text_Line">').append('<input type=text  id="table-body__name-line"class="search-input search-input__one-cell " >'), $('<div class="table-body__visible-pin">'))))
+                        .append($('<td class="table-body__cell">').append($('<div class="table-body__text-cell ">').append($('<div>').append('<input type=text id="table-body__status-line"class="search-input search-input__two-cell ">'))))
+                        .append($('<td class="table-body__cell">').append($('<div class="table-body__text-cell ">').append($('<div>').append('<input type=text id="table-body__aut-line"  class="search-input search-input__three-cell ">'))))
+                        .append($('<td class="table-body__cell">').append($('<div class="table-body__text-cell ">').append($('<div>').append('<input type=text id="table-body__login-line" class="search-input search-input__four-cell ">'))))
                         .append($('<td class="table-body__cell">').append($('<div class="table-body__img-wrapper">').append($('<div id="qwe" class="table-body__img-wrapper-item2">'))))
                     )
                 $('.table__checkbox-wrapper input:checkbox').prop('checked', false);
                 $('.table__checkbox-wrapper').not(this).removeClass("check_active");
                 $('div.table-body__name-line').addClass('table-body__name-line');
                 $.each(rowsOne, function (index, row) {
-                    $('#bossTable').children('tbody').append(row);
+                    $('#onlyTable').children('tbody').append(row);
                 });
-                $('.table-head__icons-arrow').removeClass('containerArrowSortDec');
-                $('.table-head__icons-arrow').removeClass('containerArrowSortInc');
+                $('.table-head__arrow-icon').removeClass('containerArrowSortDec');
+                $('.table-head__arrow-icon').removeClass('containerArrowSortInc');
                 counterAdditionLineage++;
             } else {
                 alert("Заполните предыдущую строку и нажмите 'ОК' ");
@@ -87,8 +87,8 @@ $(document).ready(function() {
                 var inputStatusLine = document.getElementById("table-body__name-line").value;
                 var inputAutLine = document.getElementById("table-body__name-line").value;
                 var inputLoginLine = document.getElementById("table-body__login-line").value;
-                var insertHTML = '<tr class="add_class dis"><td class="table-body__cell"><div class="table__checkbox-wrapper der"><span> <input type="checkbox" class="inp"></span></div></td><td class="table-body__cell"><div class="table-body__name-line"><div  class="text_Line">' + inputNameLine + '</div> <div class="table-body__visible-pin"></div></div></td> <td class="table-body__cell"><div class="table-body__status-line">' + inputStatusLine + '</div></td> <td class="table-body__cell"><div class="table-body__aut-line">' + inputAutLine + '</div></td > <td class="table-body__cell"><div class="table-body__login-line">' + inputLoginLine + '</div></td>  <td  class="table-body__cell"><div class="table-body__img-wrapper"><div class="table-body__img-wrapper_item"></div></div></td> </tr>'
-                $('#bossTable').append(insertHTML);
+                var insertHTML = '<tr class="add_class dis"><td class="table-body__cell"><div class="table__checkbox-wrapper der"><span> <input type="checkbox" class="inp"></span></div></td><td class="table-body__cell"><div class="table-body__text-cell font-color"><div>' + inputNameLine + '</div> <div class="table-body__visible-pin"></div></div></td> <td class="table-body__cell"><div class="table-body__text-cell">' + inputStatusLine + '</div></td> <td class="table-body__cell"><div class="table-body__text-cell">' + inputAutLine + '</div></td > <td class="table-body__cell"><div class="table-body__text-cell">' + inputLoginLine + '</div></td>  <td  class="table-body__cell"><div class="table-body__img-wrapper"><div class="table-body__img-wrapper_item"></div></div></td> </tr>'
+                $('#onlyTable').append(insertHTML);
                 $(this).addClass("table-body__img-wrapper_item");
                 $('input[type=text]').prop('disabled', true);
                 $('input[type=text]').addClass('scr');
@@ -100,7 +100,7 @@ $(document).ready(function() {
 
         });
 
-        $('#remove-lineage').click(function () {
+        $('#remove-row').click(function () {
             var flagSearch = true;
             $('tbody input:checkbox:checked').each(function () {
 
@@ -116,24 +116,24 @@ $(document).ready(function() {
                 alert('Закончите с редактированием новой строки');
             }
         });
-        $('.table-head__icons-arrow').click(function (event) {
+        $('.table-head__arrow-icon').click(function (event) {
 
             var element_click = event.target.className; // Тут имя класса по которому тыкнули
-            var parent_click_element = $('.table-head__icons-arrow').closest(".font-weight_bold");
-            var objectSearchClass = event.target.closest('.font-weight_bold');
+            var parent_click_element = $('.table-head__arrow-icon').closest(".table-head__text-cell");
+            var objectSearchClass = event.target.closest('.table-head__text-cell');
             var nameSearchClass = '.' + objectSearchClass.className;
             var repositoryIdTH = event.target.closest('th').id;// тут ID тега TH
             var id_element;
 
-            if (element_click == 'table-head__icons-arrow' || element_click == 'table-head__icons-arrow containerArrowSortDec' || element_click == 'table-head__icons-arrow containerArrowSortDec containerArrowSortInc') {
-                $('#bossTable td').removeClass('fontWe');
-                if (repositoryIdTH == 'sl') {
+            if (element_click == 'table-head__arrow-icon' || element_click == 'table-head__arrow-icon containerArrowSortDec' || element_click == 'table-head__arrow-icon containerArrowSortDec containerArrowSortInc') {
+                $('#onlyTable td').removeClass('font-weight_bold');
+                if (repositoryIdTH == 'first-column') {
                     var numberColumn = 1;
                     flag_sl++;
-                } else if (repositoryIdTH == 'nm') {
+                } else if (repositoryIdTH == 'two-column') {
                     var numberColumn = 2;
                     flag_nm++;
-                } else if (repositoryIdTH == 'km') {
+                } else if (repositoryIdTH == 'three-column') {
                     var numberColumn = 3
                     flag_km++;
                 } else {
@@ -141,28 +141,28 @@ $(document).ready(function() {
                     flag_lm++;
                 }
 
-                if (element_click == 'table-head__icons-arrow') {
+                if (element_click == 'table-head__arrow-icon') {
                     counterСlicks = 0;
-                } else if (element_click == 'table-head__icons-arrow containerArrowSortDec') {
+                } else if (element_click == 'table-head__arrow-icon containerArrowSortDec') {
                     counterСlicks = 1;
                 } else {
                     counterСlicks = 2;
                 }
 
                 if (counterСlicks == 0) {
-                    $('.table-head__icons-arrow').removeClass('containerArrowSortDec');
-                    $('.table-head__icons-arrow').removeClass('containerArrowSortInc');
+                    $('.table-head__arrow-icon').removeClass('containerArrowSortDec');
+                    $('.table-head__arrow-icon').removeClass('containerArrowSortInc');
                     constSort *= -1; //постоянная переменная работает как флаг для сортировки
                     $(event.target).addClass('containerArrowSortDec');
                     sortTable(constSort, numberColumn);
                 } else if (counterСlicks == 1) {
-                    $('.table-head__icons-arrow').removeClass('containerArrowSortInc');
+                    $('.table-head__arrow-icon').removeClass('containerArrowSortInc');
                     $(event.target).addClass('containerArrowSortInc');
                     constSort *= -1;//постоянная переменная работает как флаг для сортировки
                     sortTable(constSort, numberColumn);
                 } else {
                     $.each(rowsOne, function (index, row) {
-                        $('#bossTable').children('tbody').append(row);
+                        $('#onlyTable').children('tbody').append(row);
                     });
                     $(event.target).removeClass('containerArrowSortDec');
                     $(event.target).removeClass('containerArrowSortInc');
@@ -170,9 +170,9 @@ $(document).ready(function() {
             }
 
             function sortTable(f, n) {
-                var allRows = $('#bossTable tbody  tr').get();
+                var allRows = $('#onlyTable tbody  tr').get();
                 allRows.sort(function (a, b) {
-                    $('.table-head__icons-arrow').parents('.modifiedFont').addClass('fontWe');
+                    $('.table-head__arrow-icon').parents('.table-body__text-cell').addClass('font-weight_bold');
                     var A = getVal(a);
                     var B = getVal(b);
 
@@ -188,7 +188,7 @@ $(document).ready(function() {
                 function getVal(elm) {
                     var elementText = $(elm).children('td').eq(n).text().toUpperCase();
                     var element_fontWe = $(elm).children('td').eq(n);
-                    $(element_fontWe).addClass('fontWe')
+                    $(element_fontWe).addClass('font-weight_bold')
                     if ($.isNumeric(elementText)) {
                         elementText = parseInt(elementText, 10);
                     }
@@ -196,7 +196,7 @@ $(document).ready(function() {
                 }
 
                 $.each(allRows, function (index, row) {
-                    $('#bossTable').children('tbody').append(row);
+                    $('#onlyTable').children('tbody').append(row);
                 });
             };
         });
